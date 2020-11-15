38d0640bc12e97f00ac2583b575951a76c8f0440
DiegoGonzalez2807 <59978048+DiegoGonzalez2807@users.noreply.github.com>
2020-11-15 18:51:19 -0500

    Update Tablas.sql

--- Tablas.sql
+++ Tablas.sql
@@ -1,26 +1,27 @@
 /*CREACION DE TABLAS*/
 
 CREATE TABLE proveedor (
-    codigo     VARCHAR(8) NOT NULL,
-    telefono   VARCHAR(10) NOT NULL,
-    correo     VARCHAR(250) NOT NULL
+    codigo            VARCHAR(8) NOT NULL,
+    telefono          VARCHAR(10) NOT NULL,
+    correo            VARCHAR(250) NOT NULL,
+    direccion         VARCHAR(100) NOT NULL,
+    ciudad            VARCHAR(50) NOT NULL,
+    direccion_postal  VARCHAR(250),
+    años_experiencia  VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE persona_juridica (
-    nombre     VARCHAR(50) NOT NULL,
-    apellido   VARCHAR(50) NOT NULL,
-    cedula     VARCHAR(10) NOT NULL,
-    nit        VARCHAR(10) NOT NULL,
-    codigo     VARCHAR(8) NOT NULL
+CREATE TABLE persona_natural (
+    nombre            VARCHAR(50) NOT NULL,
+    primer_apellido   VARCHAR(50) NOT NULL,
+    segundo_apellido  VARCHAR(50) NOT NULL,
+    cedula            VARCHAR(10) NOT NULL,
+    codigo            VARCHAR(8) NOT NULL
 );
 
 CREATE TABLE empresa (
-    nombre             VARCHAR(100) NOT NULL,
-    nit                VARCHAR(10) NOT NULL,
-    codigo             VARCHAR(8) NOT NULL,
-    direccion          VARCHAR(250) NOT NULL,
-    ciudad             VARCHAR(50) NOT NULL,
-    direccion_postal   VARCHAR(20)
+    nombre  VARCHAR(250) NOT NULL,
+    nit     VARCHAR(10) NOT NULL,
+    codigo  VARCHAR(8) NOT NULL
 );
 
 CREATE TABLE provee (
@@ -35,15 +36,36 @@ CREATE TABLE bodega (
 );
 
 CREATE TABLE inventario (
-    nombre_bodega           VARCHAR(100) NOT NULL,   
-    numero_serie_pieza      NUMBER(6) NOT NULL,
+    bodega_residencia       VARCHAR(100) NOT NULL,   
     id_inventarios          VARCHAR(6) NOT NULL,
     disponibilidad          VARCHAR(13) NOT NULL
 );
 
+CREATE TABLE contiene (
+	id_inventario    VARCHAR(100) NOT NULL,
+	numero_pieza     NUMBER(6) NOT NULL
+);
+
+CREATE TABLE pieza_extraccion_petrolera (
+    numero_serie   NUMBER(6) NOT NULL,
+    tipo           VARCHAR(15) NOT NULL,
+    dimensiones    XMLTYPE
+);
+
+
 CREATE TABLE revisa (
-    id_empleado     VARCHAR(7) NOT NULL,
-    id_inventario   VARCHAR(6) NOT NULL
+    id_empleado_directo  VARCHAR(7) NOT NULL,
+    id_inventario        VARCHAR(6) NOT NULL
+);
+
+CREATE TABLE empleado_directo (
+    id                 VARCHAR(7) NOT NULL,
+    planta_residencia  XMLTYPE
+);
+
+CREATE TABLE experto (
+    id                         VARCHAR(7) NOT NULL,
+    departamento_experiencia   XMLTYPE
 );
 
 CREATE TABLE empleado (
@@ -53,47 +75,37 @@ CREATE TABLE empleado (
     cargo                  VARCHAR(100) NOT NULL,
     correo                 VARCHAR(250) NOT NULL,
     numero_telefonico      VARCHAR(20) NOT NULL,
-    departamento_trabajo   VARCHAR(20) NOT NULL,
+    departamento_trabajo   XMLTYPE,
     cedula                 VARCHAR(10) NOT NULL
-);
-
-CREATE TABLE experto (
-    id                         VARCHAR(7) NOT NULL,
-    departamento_experiencia   VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE estado (
     numero_revision   NUMBER(7) NOT NULL,
     revisado_por      VARCHAR(7) NOT NULL,
     numero_pieza      NUMBER(6) NOT NULL,
-    calidad           VARCHAR(1) NOT NULL,
-    observaciones     VARCHAR(250) NOT NULL
+    calidad           XMLTYPE,
+    observaciones     XMLTYPE
 );
-
 
 CREATE TABLE permiso (
     numero_permiso    NUMBER(7) NOT NULL,
     id_autor          VARCHAR(7) NOT NULL,
     numero_pieza      NUMBER(6) NOT NULL,
     detalle           VARCHAR(250) NOT NULL,
-    estado            VARCHAR(13) NOT NULL,
+    estado            XMLTYPE,
+	fecha_permiso     DATE NOT NULL,
     pedido            NUMBER(7)
 );
 
 CREATE TABLE pedido_pieza (
-    numero_pedido          NUMBER(7) NOT NULL,
-    id_autor               VARCHAR(7) NOT NULL,
-    departamento_trabajo   VARCHAR(20) NOT NULL,
-    cantidad_piezas        VARCHAR(5) NOT NULL,
-    bodega_reclamo         VARCHAR(20) NOT NULL
+    numero_pedido         NUMBER(7) NOT NULL,
+    departamento_destino  VARCHAR(20) NOT NULL,
+    cantidad_piezas       VARCHAR(5) NOT NULL,
+    estado                VARCHAR(13) NOT NULL,
+    bodega_reclamo        VARCHAR(20) NOT NULL,
+    fecha_pedido          DATE NOT NULL,
+    fecha_llegada         DATE NOT NULL
 );
-
-CREATE TABLE pieza_extraccion_petrolera (
-    numero_serie   NUMBER(6) NOT NULL,
-    tipo           VARCHAR(15) NOT NULL,
-    dimensiones    VARCHAR(50) NOT NULL
-);
-
 
 /*
 PRIMARY KEYS*/
